b1de259f02fb779069a4d20bd0c9b1dd509dd25e
Saito Toshinori <saito.toshinori@peco-japan.com>
2015-08-26 13:58:54 +0900

    タグ機能作成

--- app/Model/Post.php
+++ app/Model/Post.php
@@ -17,5 +17,26 @@ class Post extends AppModel {
 
     public $name = 'Post';
     public $belongsTo = 'User';
+
+    public $hasAndBelongsToMany = array(
+        'Tag' =>
+           array(
+            'classname'                     => 'Tag',
+            'joinTable'                     => 'posts_tags',
+            'foreignKey'                    => 'post_id',
+            'associationForeignKey'         => 'tag_id',//posts_tagsから変える(ドキュメントを参考に)
+            'unique'                        => 'true',
+            'conditions'                    => '',
+            'fields'                        => '',
+            'order'                         => '',
+            'limit'                         => '',
+            'offset'                        => '',
+            'finderQuery'                   => '',
+            'deleteQuery'                   => '',
+            'insertQuery'                   => ''
+        )
+
+
+    );
 }
 
