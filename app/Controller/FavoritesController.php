6690ef7515ef98d475bf57f9e31221c4b5930da1
Saito Toshinori <saito.toshinori@peco-japan.com>
2015-08-29 23:00:38 +0900

    issue6方針2

--- app/Controller/FavoritesController.php
+++ app/Controller/FavoritesController.php
@@ -3,7 +3,20 @@
 方針
 作成するアクション->add,deleteアクション
 public add function () {
-	自分のユーザーid(ログインユーザーのid)と記事のユーザーidの不一致を確かめてから
+		if (データがpostされてきたら) {
+	
+
+			if (自分のユーザーid(ログインユーザーのid)と記事のユーザーidの不一致) {
+					favoriteテーブルにデータをsave
+					setflashでseccess
+					今いるページにリダイレクト
+			} else {
+					setflashでfailed
+					今いるページにリダイレクト
+			}
+		}
+	//以下のは大まかな方針
+	自分のユーザーid(ログインユーザーのid)と記事のユーザーidの不一致の確認
 	データがpost送信されてくるのでそれをfavoritesテーブルに格納(セーブ)
 	成功->success,失敗->failed
 	現在のページヘリダイレクト
@@ -11,7 +24,19 @@ public add function () {
 
 }
 public delete function () {
-	削除する記事のidが渡されて、それに対応する記事がdeleteされる
+	id(deleteするお気に入りのidをわたし削除) {
+		タグのidを渡して削除。deleteメソッドを使うが関連するものが消えないように第二引数にはfalseを
+		setflashで成功
+		今のページヘリダイレクト
+	} else {
+		setflashで失敗
+		今のページヘリダイレクト
+
+	}
+
+
+	//以下のはおおまかな方針
+	削除するお気に入りのidが渡されて、それに対応するお気に入りがdeleteされる
 	削除が成功したらsuccessとだしてリダイレクト
 	失敗したらfailedとだしてリダイレクト
 }
